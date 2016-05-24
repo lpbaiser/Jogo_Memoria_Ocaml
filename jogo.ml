75fd37af6345e0dd71d065293ae9a4d89e386f45
lpbaiser <lpbaiser@gmail.com>
2016-05-24 01:14:17 -0300

    passos do jogo no .txt

---
+++ jogo.ml
@@ -0,0 +1,11 @@
+let _ =
+print_endline "Lançamento de dois dados";
+Random.self_init ();
+let x = 12 in
+(* let x = Random.int 6 + 1 in *)
+let y = Random.int 6 + 1 in
+
+
+(* Exemplo de print *)
+(* Printf.printf "Faces obtidas: %d e %d\n" x y *) 
+Printf.printf "Faces obtidas: %d e %d\n" y x
